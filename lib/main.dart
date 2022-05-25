e63431e1b55fca820d871e176fb26687357ced0c
V-The Windows Ninja <vnjeru85@gmail.com>
2022-05-25 23:48:15 +0300

    playing with row and column

--- lib/main.dart
+++ lib/main.dart
@@ -15,26 +15,33 @@ class HomePage extends StatelessWidget {
         title: Text("Design App"),
       ),
       body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(10),
-          width: 100,
-          height: 100,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(10),
-              gradient: LinearGradient(colors: [Colors.pink, Colors.red]),
-              boxShadow: [
-                BoxShadow(color: Colors.grey, blurRadius: 8),
-              ]),
-          child: Text(
-            "I am a box",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+          child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10),
+            width: 100,
+            height: 100,
+            alignment: Alignment.center,
+            color: Colors.red,
           ),
-        ),
-      ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            width: 100,
+            height: 100,
+            alignment: Alignment.center,
+            color: Colors.blue,
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            width: 100,
+            height: 100,
+            alignment: Alignment.center,
+            color: Colors.green,
+          ),
+        ],
+      )),
     );
   }
 }
