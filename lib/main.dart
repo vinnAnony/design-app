bd7535cefa22c01f82243feb5205923d23d68f98
V-The Windows Ninja <vnjeru85@gmail.com>
2022-05-26 20:52:58 +0300

    add user account drawer and list tiles

--- lib/main.dart
+++ lib/main.dart
@@ -25,15 +25,34 @@ class HomePage extends StatelessWidget {
           child: ListView(
             padding: const EdgeInsets.all(0),
             children: [
-              DrawerHeader(
-                child: Text(
-                  "Hello Designer, ",
+              UserAccountsDrawerHeader(
+                accountName: Text(
+                  "Vinn Njeru",
                   style: TextStyle(
                       fontSize: 25,
                       fontWeight: FontWeight.bold,
                       color: Colors.white),
                 ),
-                decoration: BoxDecoration(color: Colors.purple),
+                accountEmail: Text(
+                  "info@vinnjeru.com",
+                  style: TextStyle(color: Colors.white),
+                ),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://images.unsplash.com/photo-1472646189317-418747480cd6?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687"),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text("Vinn Njeru"),
+                subtitle: Text("Developer"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.email),
+                title: Text("Email"),
+                subtitle: Text("info@vinnjeru.com"),
+                trailing: Icon(Icons.edit),
               ),
             ],
           ),
